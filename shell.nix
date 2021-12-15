8a4388524a58923b74b6de86d417038b48d2909b
justinwoo <moomoowoo@gmail.com>
2021-12-15 15:02:29 +0200

    update

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "5716cd791c999b3246b4fe173276b42c50afdd8d";
+      sha256 = "1r9lx4xhr42znmwb2x2pzah920klbjbjcivp2f0pnka7djvd2adq";
+    }) {
+    inherit pkgs;
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    easy-ps.purs
+    easy-ps.psc-package
+  ];
+}
